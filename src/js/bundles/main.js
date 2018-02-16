10bd2ac78abf229b05edebd34d7f84f4d7eda870
Cristian Colorado <ccoloradoc@gmail.com>
2018-02-16 11:57:22 -0600

    Fix: API Key name

--- src/js/bundles/main.js
+++ src/js/bundles/main.js
@@ -80,12 +80,12 @@ class View {
       });
     }
     // Inform selection to google analytics select_content 	items, promotions, content_type, content_id
-    gtag('select_content', 'UA-92111911-1', { items: [state.name], content_type: 'state', content_id: stateId });
+    if(gtag) gtag('select_content', 'UA-92111911-1', { items: [state.name], content_type: 'state', content_id: stateId });
   }
 }
 
 let opts = {
-  api: '<Insert your Google Maps API key>'
+  key: '<Insert your Google Maps API key>'
 };
 
 // Awaiting for google api to load
